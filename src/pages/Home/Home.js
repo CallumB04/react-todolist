807f2a84d27db570b6c9cb71b3438b96cba61ca2
Cal <callumburgoyne04@gmail.com>
2024-11-10 20:04:06 +0000

    chore: remove button test

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,14 +4,9 @@ import React from 'react';
 
 function Home() {
 
-    const [count, setCount] = React.useState(0);
-
     return (
         <main className="main">
             <h1>Home Page</h1>
-
-            <button onClick={() => setCount(count + 1)} type="button">Click me!</button>
-            <p>{count}</p>
         </main>
     );
 }
