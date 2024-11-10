0941fe33bd21a077804da4fabe33fecad7ab56d6
Cal <callumburgoyne04@gmail.com>
2024-11-10 18:36:30 +0000

    test: add counter button

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,18 @@
+
 import "./Home.css"
 import React from 'react';
 
 function Home() {
 
+    const [count, setCount] = React.useState(0);
+
     return (
-        <h1>Home Page</h1>
+        <main className="main">
+            <h1>Home Page</h1>
+
+            <button onClick={() => setCount(count + 1)} type="button">Click me!</button>
+            <p>{count}</p>
+        </main>
     );
 }
 
