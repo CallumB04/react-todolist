e02a512f40ce3afdc37fd33bb201ef61e9b780de
Cal <callumburgoyne04@gmail.com>
2024-11-12 01:15:52 +0000

    feat: style home page button

--- src/pages/Home/components/HomePageBtn.js
+++ src/pages/Home/components/HomePageBtn.js
@@ -6,7 +6,9 @@ const HomePageBtn = () => {
     return (
         <Link to="/todolist">
             <button type="button" id="home-page-btn">
-                <p id="home-page-btn-text">Get Started</p>
+                <p id="home-page-btn-text">
+                    Get Started <i class="fa-solid fa-arrow-right"></i>
+                </p>
             </button>
         </Link>
     );
