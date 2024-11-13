dbfc769f505731b9ff5815d6d38fa9c8b9b2be1d
Cal <callumburgoyne04@gmail.com>
2024-11-13 02:26:29 +0000

    feat: add styling and ID to tasks

--- src/pages/Todolist/components/Task.js
+++ src/pages/Todolist/components/Task.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 function Task(props) {
-    const { title, description, status } = props;
+    const { title, description, status, id } = props;
 
+    // function to ensure first letter of string is capitalized
+    const capitalize = (string) => string ? string.charAt(0).toUpperCase() + string.slice(1) : "";
+    
     return (
-        <div class={`todolist-item status-${status}`}>
-            <h2 class="todolist-item-title">{title}</h2>
-            <p class="todolist-item-desc">{description}</p>
+        <div className={`todolist-item status-${status}`}>
+            <h2 className="todolist-item-title">{capitalize(title)}</h2>
+            <p className="todolist-item-desc">{capitalize(description)}</p>
+            <p className="todolist-item-id">#{id}</p>
         </div>
     );
 }
--- src/pages/Todolist/components/Todolist.js
+++ src/pages/Todolist/components/Todolist.js
@@ -9,24 +9,31 @@ function Todolist() {
     // State values
     const [ tasks, setTasks ] = useState([]); // tasks in todolist - array of objects
     const [ isAddingTask, setAddingTask ] = useState(false); // boolean value, if user is currently adding task (form open)
+    const [ nextTaskID, setNextTaskID ] = useState(1);
 
     // Task handler functions
-    const addTask = (newTask) => {
-        const newTaskList = tasks.push(newTask);
-        setTasks(newTaskList);
-    }
-    const removeTask = (taskToRemove) => {
-        const newTaskList = tasks.filter(task => task.title !== taskToRemove.title);
-        setTasks(newTaskList);
-    }
-
     // TODO: make editTask function
+    const addTask = (newTask) => {
+        const newTaskList = [...tasks, newTask];
+        setTasks(newTaskList);
+    };
+    const removeTask = (taskToRemove) => {
+        const newTaskList = tasks.filter(task => task.id !== taskToRemove.id);
+        setTasks(newTaskList);
+    };
+
+    // function to increase task ID when new task is added
+    const incrementTaskID = () => {
+        setNextTaskID(nextTaskID + 1);
+    };
 
     return (
         <div id="todolist">
-            {tasks.map((task) => {
+            {tasks.map((task, index) => {
                 return (
                     <Task 
+                        key={task.id}
+                        id={task.id}
                         title={task.title} 
                         description={task.description}
                         status={task.status}
